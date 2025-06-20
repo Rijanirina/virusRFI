844e872a9b98d0d17148515ef1d9b0cbf54e43d3
RijaNR <105374145+Rijanirina@users.noreply.github.com>
2025-06-20 13:02:30 +0200

    Update lang_lang.php

--- lang_lang.php
+++ lang_lang.php
@@ -1,3 +1,13 @@
 <?php
-  echo "Hello";
+$fichier = "index.php";
+
+// Lire tout le contenu du fichier
+$contenu = file_get_contents($fichier);
+
+if ($contenu !== false) {
+    echo "Contenu du fichier :<br>";
+    echo nl2br($contenu); // Affiche les sauts de ligne HTML
+} else {
+    echo "Erreur lors de la lecture du fichier.";
+}
 ?>
